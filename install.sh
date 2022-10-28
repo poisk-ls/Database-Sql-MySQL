821530aa9475778a042f976aa674da7d0a39b47b
Jade <112060601+poisk-ls@users.noreply.github.com>
2022-10-29 01:41:40 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,83 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo $red
+echo ""
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" 
+echo " Made this packages by poisk-ls🥀💛 "
+echo " add me on FB Jade Nelson Buhayan"
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+echo ""
+echo " "
+echo " "
+pkg install update -y
+echo " "
+echo " "
+echo " "
+echo ""
+echo ""
+echo "Installing ruby...."
+pkg install ruby -y
+echo " "
+echo " "
+echo " "
+echo " "
+echo "Installing lolcat...."
+gem install lolcat
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo " Installing mariadb...." | lolcat
+pkg install mariadb -y
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo " Installing php.... " | lolcat
+pkg install php -y
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo " Installing nodejs " | lolcat
+pkg install nodejs -y
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo " Installing phpmyadmin " | lolcat
+pkg install phpmyadmin -y
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "       ⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" | lolcat
+echo "         Made by Jade Nelson Buhayan🥀🔥 "   | lolcat
+echo "       ⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" | lolcat
+echo ""
+echo ""
+echo ""
+echo "      - Now it's done keep safe💛.. " | lolcat
+echo ""
+echo "      - Enjoy creating database😍" | lolcat
+echo ""
+echo "      - You can start database by typing mysqld_safe -u root" | lolcat
+echo ""
+echo "      - On your terminal slide left, open new session type mysql" | lolcat
+echo ""
+echo "      - type SHOW DATABASES" | lolcat
+echo ""
+echo "      - Now it's your turn.." | lolcat
+echo ""
+echo "      - Do it on your own risks" | lolcat
+echo ""
+echo ""
+echo "
+"
+#done
+exit
