e3ae6d07294779db1d4759be71ab0a1f51cab49e
Jade <112060601+poisk-ls@users.noreply.github.com>
2022-10-28 10:57:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,44 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+echo $red
+echo ""
 echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" 
 echo " Made this packages by poisk-ls🥀💛 "
 echo " add me on FB Jade Nelson Buhayan"
 echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
-
+echo ""
 echo " " 
 echo " " 
 pkg install update -y
-
-echo " "
 echo " " 
+echo " "
+echo " "
+
+echo "Installing ruby...."
 pkg install ruby -y
 echo " "
-
-echo " "
 echo " " 
+echo " "
+echo " "
+echo "Installing lolcat...."
 gem install lolcat
 echo ""
 echo " " 
 echo " "
 echo " " 
+
 echo " Installing mariadb...." | lolcat
 pkg install mariadb -y
 echo " " 
 echo " " 
 echo " " 
+
 echo " Installing php.... " | lolcat
 pkg install php -y
 echo " " 
 echo " " 
 echo " " 
+
 echo " Installing nodejs " | lolcat
 pkg install nodejs -y
 echo ""
@@ -41,10 +49,14 @@ pkg install phpmyadmin -y
 echo ""
 echo ""
 echo ""
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" 
+echo " Made by Jade Nelson Buhayan🥀🔥 "
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
 echo ""
-echo " Now it's done keep safe.. " | lolcat
 echo ""
-echo " Enjoy creating database" | lolcat
+echo " Now it's done keep safe💛.. " | lolcat
+echo ""
+echo " Enjoy creating database😍" | lolcat
 
 echo ""
 exit
