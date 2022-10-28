1d353a2e23bca4dbb7ce2b121742c48e06b4d223
Jade <112060601+poisk-ls@users.noreply.github.com>
2022-10-28 10:30:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,24 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶" 
+echo " made this packages by poisk-ls🥀💛 "
+echo "⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶"
+
+echo " " 
 pkg install mariadb -y
+
+echo " " 
+
 pkg install php -y
+
+echo " " 
+
 pkg install nodejs -y
+
+echo " "
+
 pkg install phpmyadmin -y
+
+echo " done " 
+
+exit
